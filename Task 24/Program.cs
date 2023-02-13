ee536ea593b60306cbfa49771e15f5ef3486c886
Лебедева Ирина <gammi_i@mail.ru>
2023-02-13 21:30:07 +0300

    На входе число А, на выходе выдает сумму чисел от 1 до А

---
+++ Task 24/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+int GetSumm(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i; // sum = sum + 1
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {N} равна {GetSumm(N)}");
