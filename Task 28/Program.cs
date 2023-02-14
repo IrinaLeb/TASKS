255eb87534fde425d70ff681d490e3f2a2fb6ae2
Лебедева Ирина <gammi_i@mail.ru>
2023-02-14 21:12:33 +0300

    На входе число N, на выходе произведение чисел от 1 до N

---
+++ Task 28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+
+int Multi(int Num)
+{
+    int prod = 1;
+    for (int i = 2; i <= Num; i++)
+    {
+        prod *= i; //prod = prod * i;
+    }
+    return prod;
+}
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Multi(N)}");
