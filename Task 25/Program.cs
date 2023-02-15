082e8b2f1498c714f080922b8f94f5706226e5d8
Лебедева Ирина <gammi_i@mail.ru>
2023-02-15 20:25:28 +0300

    На входе два числа (A и B), на выходе возводит число A в натуральную степень B

---
+++ Task 25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int Erect(int C, int D)
+{
+    int Degree = 1;
+    for (int i = 1; i <= D; i++)
+    {
+        Degree *= C; //Degree = Degree * C;
+    }
+    return Degree;
+}
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {A} в степени {B} -> {Erect(A, B)}");
