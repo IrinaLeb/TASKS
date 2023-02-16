13705e5be6257f944fd6fe2bbcde26452de64342
Лебедева Ирина <gammi_i@mail.ru>
2023-02-16 19:01:25 +0300

    На входе одномерный массив, заполненный случайными числами, на выходе сумма элементов, стоящих на нечетной позиции

---
+++ Task 36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на 
+// нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] FillArray(int size, int MinValue, int MaxValue)
+{
+    int[] Result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = new Random().Next(MinValue, MaxValue + 1);
+    }
+    return Result;
+}
+
+int SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+Console.Write("[ ");
+int[] array = FillArray(10, 100, 1000);
+Console.Write(string.Join(", ", array));
+int sum = SumArray(array);
+Console.Write(" ]");
+Console.Write($" -> {sum}");
+
